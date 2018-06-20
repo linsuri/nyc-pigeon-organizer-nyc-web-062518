56744ff5d21229d97fbc9db504ed4c81ebaf9561
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 23:31:17 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,31 +19,3 @@ def nyc_pigeon_organizer(data)
 end
 =end
 
-def nyc_pigeon_organizer(data)
-   new_pigeon = {}
-   data.each_pair do |key, value|
-     # keys color, gender, lives
-     # value => {
-     #   :purple => ["Theo", "Peter Jr.", "Lucky"],
-     #   :grey => ["Theo", "Peter Jr.", "Ms .K"],
-     #   :white => ["Queenie", "Andrew", "Ms .K", "Alex"],
-     #   :brown => ["Queenie", "Alex"]
-     # }
-     value.each_pair do |attribute, name_array|
-       # attribute => purple, grey
-       # name array => ["Theo", "Peter Jr.", "Lucky"]
-       name_array.each do |name|
-         # "theo"
-         if new_pigeon[name].nil?
-           new_pigeon[name] = {}
-         end
-         # new_pigeon = {"theo" => {:color => []}}
-         if new_pigeon[name][:color].nil?
-           new_pigeon[name][:color] = []
-         end
-         new_pigeon[name][key] << attribute
-       end
-     end
-   end
-   new_pigeon
-end
