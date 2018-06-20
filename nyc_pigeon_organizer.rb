173f1b21f3e45db99b67ea487aa563eecd5f2776
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 23:05:28 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, choice_hash|
+    choice_hash.each do |choice, names_array|
+      names_array.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {data[]}
+          
+        
 end
