6806c43af36995c5f93c60650629d348db349936
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 23:31:25 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,3 +19,14 @@ def nyc_pigeon_organizer(data)
 end
 =end
 
+def nyc_pigeon_organizer(data)
+  data.each_with_object({}) do |(key, h), result|
+    h.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
+  end
+end
